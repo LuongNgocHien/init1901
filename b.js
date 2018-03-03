f039537bdf63928b0a4e74a076ded75883625d75
kenhy <khienluongngoc1995@gmail.com>
2018-03-03 22:15:45 +0700

    first commit

---
+++ b.js
@@ -0,0 +1,15 @@
+/*
+   dynamic types
+   static types
+*/
+
+let a= 'ngoc hien';
+let b='luong '
+console.log(b +a);
+/*
+   primitive : string , number, boolean undefine, null
+   object: object , functions, arrays
+ */
+
+a=3;
+console.log(a);
---
+++ index.js
@@ -0,0 +1,17 @@
+d=8;
+var a=1;
+let b=2;
+const c=3;
+var d=7;
+
+b=3;
+// b=10;
+// c=10;
+
+// console.log(a,b,c);
+console.log(b);
+// console.log(c);
+// if(true){
+//     var d=1;
+// }
+console.log(d);
